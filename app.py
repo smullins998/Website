9a24925b0d710c18b56e0a7e82106312409253dd
sean mullins <seanmullins333@seans-mbp.myfiosgateway.com>
2023-05-16 09:49:02 -0400

    update

--- app.py
+++ app.py
@@ -5,20 +5,6 @@ import random
 app = Flask(__name__)
 app.debug = True
 
-#Here is the creation of the database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
-db = SQLAlchemy(app)
-
-class response(db.Model):
-    _id = db.Column('id', db.Integer, primary_key=True)
-    name = db.Column('name', db.String(100))
-    resp = db.Column('response', db.String(500))
-  
-    def __init__(self, name, resp):
-        self.name = name
-        self.resp = resp
-
-
 #Backend 
 @app.route('/')
 def home():
@@ -27,13 +13,6 @@ def home():
 @app.route('/portfolio')
 def portfolio():
     return render_template('portfolio.html')
-
-
-
-
-
-with app.app_context():
-    db.create_all()
 
 if __name__ == '__main__':
     app.run(port=8090)
