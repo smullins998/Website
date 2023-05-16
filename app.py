32dafd5cff34cb360b05a56aff22225cb8e0f952
sean mullins <seanmullins333@seans-mbp.myfiosgateway.com>
2023-05-16 10:10:36 -0400

    update

--- app.py
+++ app.py
@@ -13,6 +13,6 @@ def home():
 @app.route('/portfolio')
 def portfolio():
     return render_template('portfolio.html')
-#
+
 if __name__ == '__main__':
-    app.run(port=8090)
+    app.run(host='0.0.0.0', port=8090)
