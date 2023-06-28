ffdec51f773df3aa0e4a1fdcfcd586646a541de1
Sean Mullins <Smullins998@gmail.com>
2023-06-28 16:38:11 -0400

    Update

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request, session
 import random
 
+from ProbabilityChart import RunPlot
+from ProbabilityChart import MLP, KNN, RF
+
+
 
 app = Flask(__name__)
 app.debug = True
@@ -8,7 +12,10 @@ app.debug = True
 #Backend 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    
+    Prob_fig = RunPlot(MLP())
+    
+    return render_template('home.html', Prob_fig=Prob_fig)
  
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8090)
